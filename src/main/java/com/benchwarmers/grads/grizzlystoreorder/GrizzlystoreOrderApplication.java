b79d95340bf987983ec0ea7811ba5489ff171469
a-nthonymai <Anthony.Mai@hotmail.com>
2018-09-04 11:25:25 +1000

    created initial project template

---
+++ src/main/java/com/benchwarmers/grads/grizzlystoreorder/GrizzlystoreOrderApplication.java
@@ -0,0 +1,12 @@
+package com.benchwarmers.grads.grizzlystoreorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrizzlystoreOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrizzlystoreOrderApplication.class, args);
+    }
+}
